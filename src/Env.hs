426c859c1186ddef4de38937e0abce89cae07599
Alexander Shestakov <a.shestakov@itransition.com>
2020-02-05 22:27:49 +0300

    HD-222217 Allowing to fail nicely

--- src/Env.hs
+++ src/Env.hs
@@ -6,12 +6,14 @@ module Env where
 import           Data.Text          ( Text, pack, unpack )
 
 import           System.Environment ( lookupEnv )
+import           System.Exit        ( exitFailure )
 
 readEnv :: String -> IO Text
 readEnv name = lookupEnv name >>= \case
   Just value -> return $ pack value
   Nothing    -> do
-    error $ "Please set " ++ name ++ " evironment variable."
+    putStrLn $ "Please set " ++ name ++ " evironment variable."
+    exitFailure
 
 readPort :: (Integral a, Read a) => String -> IO a
 readPort name = read . unpack <$> readEnv name
