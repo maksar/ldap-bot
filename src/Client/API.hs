2d9afb5d370b81e2be44e2c0451ab76297d68256
Shestakov, Aleksandr <a.shestakov@itransition.com>
2020-02-06 20:45:47 +0300

    HD-222217 Fixing typo

--- src/Client/API.hs
+++ src/Client/API.hs
@@ -59,7 +59,7 @@ sendHelpMessage token recipient = do
               { "title": "To list members of a group, try command:", "subtitle": "/list of ITRBY.Management", "buttons": [{"title": "/list of ITRBY.Management", "type": "postback", "payload": "/list of ITRBY.Management"}] },
               { "title": "To add a person to a group, try command:", "subtitle": "/add a.person to ITRBY.Management", "buttons": [{"title": "/add a.person to ITRBY.Management", "type": "postback", "payload": "/add a.person to ITRBY.Management"}] },
               { "title": "To remove a person from a group, try command:", "subtitle": "/remove a.person from ITRBY.Management", "buttons": [{"title": "/remove a.person from ITRBY.Management", "type": "postback", "payload": "/remove a.person from ITRBY.Management"}] },
-              { "title": "To get this help message, user command:", "buttons": [{"title": "/help", "type": "postback", "payload": "/help"}] } ] } } } }
+              { "title": "To get this help message, use command:", "buttons": [{"title": "/help", "type": "postback", "payload": "/help"}] } ] } } } }
     |]) (mkClientEnv manager graphAPIBaseUrl)
   return $ Right ()
 
