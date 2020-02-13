4135da9306d418e647ecee70aee996bdb7be2edb
Shestakov, Aleksandr <a.shestakov@itransition.com>
2020-02-13 21:44:15 +0300

    HD-222217 Adding facebook tests

--- src/Server/Hook.hs
+++ src/Server/Hook.hs
@@ -1,16 +1,21 @@
 {-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE FlexibleContexts  #-}
+{-# LANGUAGE GADTs             #-}
+{-# LANGUAGE LambdaCase        #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE TypeOperators     #-}
 
 module Server.Hook (
   webhookMessage
 ) where
 
-import           Control.Monad.Freer        ( Eff, runM, subsume )
+import           Control.Monad.Freer        ( Eff, reinterpret, runM, subsume )
 import           Control.Monad.Freer.Error  ( Error, runError )
 import           Control.Monad.Freer.Reader ( Reader, runReader )
+import           Control.Monad.Freer.Writer
 import           Control.Monad.IO.Class     ( liftIO )
 
-import           Data.Text                  ( Text )
+import           Data.Text                  ( Text, pack, unpack )
 import           Data.Vector                ( Vector, mapM )
 import           Prelude                    hiding ( mapM )
 
@@ -23,8 +28,12 @@ import           Env
 import           Server.LDAP
 import           Server.Model
 
-webhookMessage :: Config -> Messages -> Handler (Vector (Either Text SendTextMessageResponse))
-webhookMessage config (Messages inputs) = mapM (liftIO . execute config . bot) inputs
 
-execute :: Config -> Eff '[LdapEffect, FacebookEffect, Error Text, Reader Config, IO] a -> IO (Either Text a)
-execute config = runM . runReader config . runError . subsume . runFacebook . subsume . runLdap
+
+import           Control.Monad              ( join )
+
+webhookMessage :: Config -> Messages -> Handler (Vector (Either Text SendTextMessageResponse))
+webhookMessage config (Messages inputs) = mapM (liftIO . interpretProgram config . program facebookProgram ldapProgram) inputs
+
+interpretProgram :: Config -> Eff '[LdapEffect, FacebookEffect, Error Text, Reader Config, IO] a -> IO (Either Text a)
+interpretProgram config = runM . runReader config . runError . subsume . runFacebook . subsume . runLdap
