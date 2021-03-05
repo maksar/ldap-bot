15e7ef78ae13621735bf7bdad001353f152ae9d9
Shestakov, Aleksandr <a.shestakov@itransition.com>
2021-03-05 16:54:46 +0300

    HD-222217 Adding history

--- app/Console.hs
+++ app/Console.hs
@@ -3,29 +3,47 @@ module Main where
 import App (botConsole, runBot)
 import Control.Monad (join)
 import Control.Monad.IO.Class (MonadIO (liftIO))
+import Data.List (isPrefixOf, tails)
 import Data.Text (pack, unpack)
 import Main.Utf8 (withUtf8)
 import System.Console.Haskeline
-  ( defaultSettings,
+  ( Settings,
+    completeWord,
+    defaultSettings,
     getInputLine,
+    historyFile,
     outputStrLn,
     runInputT,
+    setComplete,
+    simpleCompletion,
   )
 import System.Console.Pretty
   ( Color (..),
     color,
   )
+import System.Directory (getHomeDirectory)
+
+settings :: MonadIO m => FilePath -> Settings m
+settings home =
+  setComplete
+    ( completeWord
+        Nothing
+        []
+        ( \start ->
+            pure (map simpleCompletion $ filter (any (start `isPrefixOf`) . tails) ["/list", "/add", "/remove"])
+        )
+    )
+    $ defaultSettings {historyFile = Just (home <> "/.ldap-bot-console")}
 
 main :: IO ()
 main = withUtf8 $ do
   putStrLn $ color Green "Ldap-bot is listening on terminal"
-
-  runInputT defaultSettings loop
+  home <- getHomeDirectory
+  runInputT (settings home) loop
   where
     loop = do
       getInputLine (color Blue ">> ") >>= \case
         Nothing -> return ()
-        Just ":q" -> return ()
         Just "" -> loop
         Just input -> do
           message <-
